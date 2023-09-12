08f888b4fa5a10b3b80886ec1cbbdabb0b774f38
Elvan Ersöz <elvanersoz@outlook.com>
2023-09-12 13:25:56 +0300

    file locations and naming fixed

--- employeeControlBackend/personelkontrol/src/main/java/com/example/personelkontrol/repos/IzinSebepRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.personelkontrol.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.personelkontrol.entities.IzinSebep;
-
-public interface IzinSebepRepository  extends JpaRepository<IzinSebep, Integer>{
-
-}
